5978259a0b94761f137d6bdba16d5741306aa776
lifengss <lifeng_ss@hotmail.com>
2022-06-30 10:17:38 +0800

    update hidden PPG

--- yuan_api/__init__.py
+++ yuan_api/__init__.py
@@ -1,4 +1,5 @@
 import sys
 import os
-
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+from yuan_api.inspurai import set_yuan_account
+
--- yuan_api/examples/hidden_poetry.py
+++ yuan_api/examples/hidden_poetry.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.abspath(os.curdir))
 from yuan_api.inspurai import Yuan, set_yuan_account,Example
 
 # 1. set account
-set_yuan_account("账号", "手机号")  # 输入您申请的账号和手机号
+# set_yuan_account("账号", "手机号")  # 输入您申请的账号和手机号
 
 # 2. initiate yuan api
 # 注意：engine必需是['base_10B','translate','dialog']之一，'base_10B'是基础模型，'translate'是翻译模型，'dialog'是对话模型
@@ -25,9 +25,9 @@ yuan.add_example(Example(inp="以清风为题作一首诗：“",
                         out="春风用意匀颜色，销得携觞与赋诗。秾丽最宜新著雨，娇饶全在欲开时。”"))
 
 
-def hidden_poetry(prompt,yuan):
+def hidden_poetry(prompt,yuan,theme):
 
-    header = '以家国' + '为题作一首诗：“'
+    header = '以' + theme + '为题作一首诗：“'
     res = ''
     poem = ''
     head_chs = list(prompt)
@@ -38,13 +38,17 @@ def hidden_poetry(prompt,yuan):
         except:
             ch = ''
         query = header + poem + ch
-        while 1:
+        for j in range(3):
+            no_resp = True
             res = yuan.submit_API(query,trun='”')
             res = res.replace('。','，')
             res = res.split('，')
-            if len(res[0]) == 6:
+            if len(res[0]) >= 7-len(ch):
+                no_resp = False
                 break
-        res = res[0]
+        if no_resp:
+            return "题目有些难，模型无输出"
+        res = res[0][:7-len(ch)]
         if i%2:
             pun = '。'
         else:
@@ -59,9 +63,9 @@ while(1):
     prompt = input("以何为题作诗：")
     if prompt.lower() == "q":
         break
-    
+    theme = input('作诗主题：')
     # 藏头诗逻辑
-    response = hidden_poetry(prompt,yuan)
+    response = hidden_poetry(prompt,yuan, theme)
     
     # response = yuan.submit_API(prompt=prompt,trun="”")
     print('\n'+response)
